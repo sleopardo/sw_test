6a941a4a1981519bdf57c40dd1c9e3adfc9135f7
sleopardo <santiagoleopardo@gmail.com>
2015-07-15 22:18:12 -0300

    fixes in url scope

--- sw/index.js
+++ sw/index.js
@@ -4,7 +4,7 @@ console.log('Boot app!');
 
 if (navigator.serviceWorker) {
   navigator.serviceWorker
-    .register('./sw.js', { scope: '/sw' })
+    .register('./sw.js', { scope: '/sw_test/sw' })
     .then(function(reg) {
       // registration worked
       console.log('Registration succeeded. Scope is ' + reg.scope);
